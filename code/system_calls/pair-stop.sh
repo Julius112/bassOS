efc63f64135c7098ad4c00019e94c4d96b0bfb9b
Julius Niedworok <julius.n@gmx.net>
2017-09-10 20:58:01 +0000

    Removed code as it is moved to a different repository

--- code/system_calls/pair-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -f /var/run/bluetooth_pair.pid ]; then
-    kill -9 `cat /var/run/bluetooth_pair.pid`
-    rm /var/run/bluetooth_pair.pid
-fi
--- code/www/controllers/playlistController.js
+++
@@ -1,3 +0,0 @@
-angular.module('bassOS').controller('playlistCtl', function($scope) {
-	$scope.bluetooth_visability = false;
-}); 
