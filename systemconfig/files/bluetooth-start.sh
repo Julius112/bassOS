94eaa453201e6dd0d375f753bfc5f63189f87482
Julius Niedworok <julius.niedworok@uk.ibm.com>
2017-09-28 15:34:08 +0100

    Reflected BassOS backend service API changes in support scripts

--- systemconfig/files/bluetooth-start.sh
+++ systemconfig/files/bluetooth-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -H 'Content-Type: application/json' -X PUT -d '{"service":"bluetooth","playback":"true"}' localhost:3000/playback
+curl -H 'Content-Type: application/json' -X PUT -d '{"playback":"true"}' localhost:3000/services/bluetooth
--- systemconfig/files/bluetooth-stop.sh
+++ systemconfig/files/bluetooth-stop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -H 'Content-Type: application/json' -X PUT -d '{"service":"bluetooth","playback":"false"}' localhost:3000/playback
+curl -H 'Content-Type: application/json' -X PUT -d '{"playback":"false"}' localhost:3000/services/bluetooth
